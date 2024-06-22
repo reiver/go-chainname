ea0d330d03b639c1e49ea300f765d231a485c67c
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-06-22 04:36:54 -0700

    Ethereum Classic

---
+++ 61.go
@@ -0,0 +1,14 @@
+package chaincname
+
+import (
+	"github.com/reiver/go-chainid"
+)
+
+const EthereumClassic = "Ethereum Classic"
+
+func init() {
+	const value string =                   EthereumClassic
+	var   key   string = createkey(chainid.EthereumClassic)
+
+	chainNames[key] = value
+}
